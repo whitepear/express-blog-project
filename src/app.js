19fe1f71fe8ae88aa29f01b8f7beadf3211232d0
X Y <webdevnorth@gmail.com>
2016-05-15 23:14:16 +0000

    added parameter logic to '/blog' get route

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ app.get('/', function (req, res) {
     res.send('<h2>This is the root.</h2>');
 });
 
-app.get('/blog', function (req, res) {
-    res.send(posts);
+app.get('/blog/:title', function (req, res) {
+    var title = req.params.title;
+    var post = posts[title];
+    res.send(post);
 });
 
 app.listen('8080', function() {
