b37fd1d669c8cfc06806bdfa23201a521186dad3
X Y <webdevnorth@gmail.com>
2016-05-16 13:58:45 +0000

    added blog landing page template functionality to app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@
 var express = require('express');
 var posts = require('./mock/posts.json');
 
+var postsList = Object.keys(posts).map(function (value) {
+    return posts[value];
+}); // generate an array of objects 
+
 var app = express();
 
 app.use('/static', express.static(__dirname + '/public')); // use defines middleware -- logic for handling request before it arrives at a route
@@ -17,7 +21,7 @@ app.get('/', function (req, res) {
 app.get('/blog/:title?', function (req, res) { // : indicates parameter, ? indicates parameter is optional
     var title = req.params.title; // store title param
     if (title === undefined) { // if empty, send message below
-        res.send('This page is under construction.');
+        res.render('blog', { posts: postsList});
     } else { // if not empty, use param to locate post in json and store in variable
         var post = posts[title] || {}; // if post we're looking for does not exist (because param is junk) define it as an empty object
         res.render('post', { post: post }); // render post.jade (1st arg), assign post variable to post property on second arg object -- this allows access to post in template via the post property  
